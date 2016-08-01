462f14649eea7644a2e70c1bb77e052bc3207859
sn0wfree <snowfreedom0815@gmail.com>
2016-08-01 15:30:27 +0100

    update3.1

--- personalcode/ocrtest2.1.py
+++ personalcode/ocrtest2.1.py
@@ -10,14 +10,13 @@ this code is for my dissertation.
 
 
 __author__='sn0wfree'
-__version__='2.0.4.1'
+__version__='2.1.4.1'
 
 
 # when i begin to write this codes, i truely understand what i have done
 # but now i'm not
 #-----------------------------------------------------------------------------------------------
 ###
-
 import os,gc,sys
 import pandas as pd
 import unicodecsv
@@ -949,6 +948,16 @@ def progress_test(counts,lenfile,speed,w):
 
     #sys.stdout.write("\rPercent: [%s] %d%%,remaining time: %.4f mins"%(hashes + spaces,precent,w))
     sys.stdout.flush()
+def multi_scanfolderprocess(a):
+    pool=mp.Pool()
+    f_a=pool.map(scanfolderprocess,(a,))
+    #print f_a
+    if len(f_a)==1:
+        f=f_a[0]
+    elif len(f_a)!=1:
+        f=f_a
+    pool.close()
+    return f
 
 if __name__ == '__main__':
     global characteristiclibs
@@ -1019,7 +1028,13 @@ if __name__ == '__main__':
     #common_headers=['Project_ID','0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100']
     gc.enable()
 
-    daily_data_collection_main(rdirs,y)
+    #daily_data_collection_main(rdirs,y)
+    pool = mp.Pool()
+    pool.map(total_scan_recognization_regroup_process, rdirs)
+    #for rdir in rdirs:
+    #    total_scan_recognization_regroup_process(rdir)
+    
+
 
 
 
--- personalcode/tt.py
+++ personalcode/tt.py
@@ -35,99 +35,57 @@ def scanfolderprocess(a):
     return f
 
 
-
-def cropdailybar(dicts,dicts_axis,image):
-    a=sorted(list(dicts))
-    
-    daylabel={}
-    for i in xrange(len(dayslabel)):
-        daylabel[dayslabel[i]]=i
-    #print a
-
-
-    counts=0
-    line={}
-    count=0
-    for i in xrange(0,len(a)):
-        if i <(lena-1):
-            if  (a[i+1]-a[i])<=4:
-                count+=1
-            else:
-                line[counts]=(a[i]-count,a[i])
-                #gap.append(a[i+1]-a[i])
-                count=0
-                counts+=1
-        else:
-            if i==(lena-1):
-                line[counts]=(a[i]-count,a[i])
-
-    loca=(a[0],0)
-    lena=len(a)
-    for i in xrange(0,lena):
-        if i <(lena-1):
-            if  (a[i+1]-a[i])<2:
-                if loca[1]<=a[i+1]:
-                    loca=(loca[0],a[i+1])
-                elif loca[1]>a[i+1]:
-                    raise Exception('image daily split process err')
-            elif (a[i+1]-a[i])>=2:
-                if loca[1]==a[i]:
-                    line.append(loca)
-                    loca=(a[i+1],a[i+2])
-                elif loca[1]<a[i]:
-                    raise Exception('image daily split process err')
-                elif loca[1]>a[i]:
-                    raise Exception('image daily split process err')
-        elif i ==(lena-1):
-            loca=(loca[0],a[i])
-            line.append(loca)
-                #return a dict
-    dailyimage={}
-    #print line
-    listline=list(line)
-    #print line[0]
-    #(a,w)=line[0]
-    #boxa=(0,a-1,image.size[0],w+2)
-    #ss=image.crop(boxa)
-    #ss.show()
-
-    for i in listline:
-        (y1,y2)=line[i]
-        average=(y1+y2)/2
-        for row in list(daylabel):
-            if row in xrange(average-2,average+2):
-                loacted_day=daylabel[row]
-                del daylabel[row]
-                break
-        box=(0,y1-1,image.size[0],y2+2)
-        locals()['data%s'%i]=image.crop(box)
-
-        dailyimage[loacted_day]=locals()['data%s'%i]
-    #dailyimage[0].show()
-    return dailyimage
-
+def multi_scanfolderprocess(a):
+    pool=mp.Pool()
+    f_a=pool.map(scanfolderprocess,(a,))
+    #print f_a
+    if len(f_a)==1:
+        f=f_a[0]
+    elif len(f_a)!=1:
+        f=f_a
+    pool.close()
+    return f
 
 if __name__=='__main__':
-    test=input('test')
-    ff1=time.time()
+    #a=[{0: 'dot', 1: 'dot', 2: 'dot', 3: 'dot', 4: 'dot', 5: 'dot', 6: 'dot', 7: 'dot', 8: 'dot', 9: 'dot', 10: 'dot', 11: 'dot', 12: 'dot', 13: 'dot', 14: 'dot', 15: 'dot', 16: 'dot', 17: 'dot', 18: 'dot', 19: 'dot', 20: 'dot', 21: 'dot', 22: 'dot', 23: 'dot', 'Project_ID': 1011352051.0}, {0: 'dot', 1: 'dot', 2: 'dot', 3: 'dot', 4: 'dot', 5: 'dot', 6: 'dot', 7: 'dot', 8: 'dot', 9: 'dot', 10: 'dot', 11: 'dot', 12: 'dot', 13: 'dot', 14: 'dot', 15: 'dot', 16: 'dot', 17: 'dot', 18: 'dot', 19: 'dot', 20: 'dot', 21: 'dot', 22: 'dot', 23: 'dot', 24: 'dot', 25: 'dot', 26: 'dot', 'Project_ID': 1012665249.0, 28: 'dot', 27: 'dot'}, {0: '1', 1: '0', 2: '0', 3: '1', 4: '0', 5: '1', 6: '0', 7: '0', 8: '0', 9: '0', 10: '0', 11: '0', 12: '0', 13: '0', 14: '0', 15: '0', 16: '0', 17: '0', 18: '0', 19: '0', 20: '0', 'Project_ID': 1012924504.0}, {0: '29', 1: '28', 2: '11', 3: '9', 4: '23', 5: '10', 6: '11', 7: '6', 8: '11', 9: '8', 10: '7', 11: '12', 12: '7', 13: '12', 14: '15', 15: '10', 16: '5', 17: '8', 18: '15', 19: '7', 20: '3', 21: '3', 22: '16', 23: '14', 24: '12', 25: '2', 26: '2', 'Project_ID': 1015375165.0, 28: '17', 29: '9', 30: '14', 31: '14', 27: '1'}, {0: '3', 1: '2', 2: '0', 3: '0', 4: '0', 5: '1', 6: '0', 7: '0', 8: '0', 9: '0', 10: '0', 11: '0', 12: '0', 13: '0', 14: '1', 15: '0', 16: '0', 17: '0', 18: '0', 19: '0', 20: '0', 21: '0', 22: '0', 23: '0', 24: '0', 25: '0', 26: '0', 'Project_ID': 1016595269.0, 28: '0', 29: '0', 30: '0', 31: '0', 32: '0', 33: '0', 34: '0', 27: '0', 36: '0', 37: '0', 38: '0', 39: '0', 40: '0', 41: '0', 35: '0'}, {0: '6', 1: '4', 2: '0', 3: '5', 4: '3', 5: '3', 6: '1', 7: '1', 8: '0', 9: '2', 10: '4', 11: '0', 12: '0', 13: '1', 14: '1', 15: '1', 16: '1', 17: '1', 18: '1', 19: '0', 20: '0', 21: '0', 22: '1', 23: '0', 24: '2', 25: '1', 26: '2', 'Project_ID': 1017118969.0, 28: '2', 29: '0', 30: '0', 27: '1'}, {0: '3', 1: '3', 2: '0', 3: '0', 4: '3', 5: '0', 6: '2', 7: '1', 8: '0', 9: '0', 10: '1', 11: '1', 12: '3', 13: '6', 14: '2', 15: '0', 16: '0', 17: '3', 18: '1', 19: '1', 20: '1', 21: '1', 22: '3', 23: '0', 24: '0', 25: '0', 26: '0', 'Project_ID': 101799307.0, 28: '4', 29: '2', 30: '0', 31: '0', 32: '0', 33: '1', 34: '0', 27: '1', 36: '1', 37: '1', 38: '2', 39: '0', 35: '2'}, {0: '1', 1: '1', 2: '0', 3: '0', 4: '0', 5: '0', 6: '0', 7: '0', 8: '0', 9: '0', 10: '0', 11: '0', 12: '0', 'Project_ID': 1019217557.0}, {0: 'dot', 1: 'dot', 2: 'dot', 3: 'dot', 4: 'dot', 5: 'dot', 6: 'dot', 7: 'dot', 8: 'dot', 9: 'dot', 10: 'dot', 11: 'dot', 12: 'dot', 13: 'dot', 14: 'dot', 15: 'dot', 16: 'dot', 17: 'dot', 18: 'dot', 19: 'dot', 20: 'dot', 21: 'dot', 22: 'dot', 23: 'dot', 24: 'dot', 25: 'dot', 26: 'dot', 'Project_ID': 1019741337.0, 28: 'dot', 29: 'dot', 30: 'dot', 27: 'dot'}, {0: '0', 1: '1', 2: '1', 3: '0', 4: '0', 5: '0', 6: '2', 7: '0', 8: '1', 9: '1', 10: '0', 11: '0', 12: '0', 13: '0', 14: '7', 15: '2', 16: '0', 17: '0', 18: '0', 19: '0', 20: '0', 21: '1', 22: '0', 23: '0', 24: '0', 25: '0', 26: '0', 'Project_ID': 1022100836.0, 28: '0', 29: '0', 30: '0', 31: '0', 32: '0', 33: '0', 34: '0', 27: '0', 36: '0', 37: '0', 38: '1', 39: '2', 40: '0', 41: '0', 42: '2', 43: '0', 44: '0', 45: '2', 46: '1', 47: '0', 48: '0', 49: '0', 50: '0', 51: '0', 52: '0', 53: '2', 54: '0', 55: '0', 56: '0', 57: '1', 58: '1', 59: '0', 60: '0', 35: '0'}]
+    #test=input('test')
+
 
 
     b=0
     a='/Users/sn0wfree/Dropbox/BitTorrentSync/data/image/file'
     #print a
-    for i in xrange(0,250):
-        c=(a,b)
-        if test ==1:
-            pool=mp.Pool()
-            f_a=pool.map(scanfolderprocess,(a,))
-            if len(f_a)==1:
-                f=f_a[0]
-            elif len(f_a)!=1:
-                f=f_a
-            pool.close()
-            #pool.join()
-        else:
-            f=scanfolderprocess(a)
-    #print f[0]
+    c=(a,b)
+    y=200
+    f=scanfolderprocess(a)
+    print len(f)
 
-    print time.time()-ff1
+
+
+'''
+    def run(x,y):
+        ff1=time.time()
+        if x ==1:
+
+            for i in xrange(0,y):
+                pool=mp.Pool()
+                f_a=pool.map(scanfolderprocess,(a,))
+
+                if len(f_a)==1:
+                    f=f_a[0]
+                elif len(f_a)!=1:
+                    f=f_a
+                pool.close()
+                #pool.join()
+        else:
+            for i in xrange(0,y):
+                f=scanfolderprocess(a)
+        #print f[0]
+
+        return (time.time()-ff1)/y
+    for x in xrange(2):
+        if x==1:
+            print 'multiprocessing_time: %s'%run(x,y)
+        else:
+            print 'normal_time: %s'%run(x,y)
+'''
